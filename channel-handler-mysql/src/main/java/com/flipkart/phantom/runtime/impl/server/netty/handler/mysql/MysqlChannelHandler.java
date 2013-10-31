ee0a2b39f35465f9c3275125aa27b2fdbe7a28d6
Saikat Maitra <saikat.maitra@gmail.com>
2013-10-31 22:56:48 +0530

    adding modules for mysql proxy

---
+++ channel-handler-mysql/src/main/java/com/flipkart/phantom/runtime/impl/server/netty/handler/mysql/MysqlChannelHandler.java
@@ -0,0 +1,11 @@
+package com.flipkart.phantom.runtime.impl.server.netty.handler.mysql;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 31/10/13
+ * Time: 12:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MysqlChannelHandler {
+}
---
+++ task-mysql/src/main/java/com/flipkart/phantom/mysql/impl/MysqlProxyExecutor.java
@@ -0,0 +1,11 @@
+package com.flipkart.phantom.mysql.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 31/10/13
+ * Time: 5:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MysqlProxyExecutor {
+}
